c78daf73eaccd28bf16a420cdf235b2e90febb44
GabriMagic <champy9a@gmail.com>
2016-12-12 13:41:49 +0000

    SDFV

--- app/src/main/java/pgl/ejercicios/generadorgift/CrearActivity.java
+++ app/src/main/java/pgl/ejercicios/generadorgift/CrearActivity.java
@@ -1,19 +1,68 @@
 package pgl.ejercicios.generadorgift;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by GabriMagic on 12/12/2016.
  */
 public class CrearActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.crear_pregunta);
 
+        PreguntasHelper preguntasHelper = new PreguntasHelper(this, "PreguntasDB", null, 1);
+        db = preguntasHelper.getWritableDatabase();
     }
+
+    public void crearPregunta(View v) {
+
+        TextView titulo = (TextView) findViewById(R.id.tituloText);
+        TextView pregunta = (TextView) findViewById(R.id.preuntaText);
+
+        TextView rel11 = (TextView) findViewById(R.id.rel1_1);
+        TextView rel12 = (TextView) findViewById(R.id.rel1_2);
+
+        TextView rel21 = (TextView) findViewById(R.id.rel2_1);
+        TextView rel22 = (TextView) findViewById(R.id.rel2_2);
+
+        TextView rel31 = (TextView) findViewById(R.id.rel3_1);
+        TextView rel32 = (TextView) findViewById(R.id.rel3_2);
+
+        TextView rel41 = (TextView) findViewById(R.id.rel4_1);
+        TextView rel42 = (TextView) findViewById(R.id.rel4_2);
+
+        Cursor c = db.rawQuery("SELECT * FROM preguntas ORDER BY id_pregunta DESC LIMIT 1", null);
+
+        int i = 0;
+        if (c.moveToFirst()) {
+
+             i = c.getInt(0);
+
+            do {
+                i++;
+            } while (c.moveToNext());
+        };
+
+
+        db.execSQL("INSERT INTO preguntas (titulo, pregunta) VALUES ('" + titulo.getText() + "','" + pregunta.getText() + "')");
+        db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel11.getText()+"','"+rel12.getText()+"')");
+        db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel21.getText()+"','"+rel22.getText()+"')");
+        db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel31.getText()+"','"+rel32.getText()+"')");
+        db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel41.getText()+"','"+rel42.getText()+"')");
+
+    }
+
+    public void cancel(View v) {
+        finish();
+    }
+
 }
--- app/src/main/java/pgl/ejercicios/generadorgift/MainActivity.java
+++ app/src/main/java/pgl/ejercicios/generadorgift/MainActivity.java
@@ -1,13 +1,19 @@
 package pgl.ejercicios.generadorgift;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView label;
+    PreguntasHelper preguntasHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +23,33 @@ public class MainActivity extends AppCompatActivity {
         Button crear = (Button) findViewById(R.id.crear);
         Button exportar = (Button) findViewById(R.id.exportar);
 
-        PreguntasHelper preguntasHelper = new PreguntasHelper(this, "PreguntasDB", null, 1);
-
-        SQLiteDatabase db = preguntasHelper.getWritableDatabase();
-
-
-        TextView label = (TextView) findViewById(R.id.label);
-
-        int pregunta = 1;
-
-        db.execSQL("INSERT INTO preguntas (titulo, pregunta) VALUES ('Animales', 'Relacionalos')");
-        db.execSQL("INSERT INTO relaciones VALUES ("+pregunta+",'oso','pardo')");
-        pregunta++;
-
-        Cursor c = db.rawQuery("SELECT * FROM preguntas", null);
-        c.moveToFirst();
-        label.setText(c.getString(1));
+        label = (TextView) findViewById(R.id.label);
+        preguntasHelper = new PreguntasHelper(this, "PreguntasDB", null, 1);
+        db = preguntasHelper.getWritableDatabase();
 
 
+        preguntasHelper.onUpgrade(db, 1, 1);
+        load(db, label);
 
+
+    }
+
+    private void load(SQLiteDatabase db, TextView label) {
+        Cursor c = db.rawQuery("SELECT * FROM relaciones", null);
+        label.setText("");
+        if (c.moveToFirst()) {
+            do {
+                label.setText(label.getText() + "" + c.getInt(0) + c.getString(1) + c.getString(2) + "\n");
+            } while (c.moveToNext());
+        }
+    }
+
+    public void crearPregunta(View v) {
+        Intent intent = new Intent(MainActivity.this, CrearActivity.class);
+        startActivity(intent);
+    }
+
+    public void exportarPregunta(View v) {
+        load(db, label);
     }
 }
