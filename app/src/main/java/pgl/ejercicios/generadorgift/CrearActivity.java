cd0164423be4cfd0c99758f0e3f76f00642c4d63
GabriMagic <champy9a@gmail.com>
2016-12-12 14:02:49 +0000

    dgh

--- app/src/main/java/pgl/ejercicios/generadorgift/CrearActivity.java
+++ app/src/main/java/pgl/ejercicios/generadorgift/CrearActivity.java
@@ -14,6 +14,21 @@ public class CrearActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
 
+    TextView titulo = (TextView) findViewById(R.id.tituloText);
+    TextView pregunta = (TextView) findViewById(R.id.preuntaText);
+
+    TextView rel11 = (TextView) findViewById(R.id.rel1_1);
+    TextView rel12 = (TextView) findViewById(R.id.rel1_2);
+
+    TextView rel21 = (TextView) findViewById(R.id.rel2_1);
+    TextView rel22 = (TextView) findViewById(R.id.rel2_2);
+
+    TextView rel31 = (TextView) findViewById(R.id.rel3_1);
+    TextView rel32 = (TextView) findViewById(R.id.rel3_2);
+
+    TextView rel41 = (TextView) findViewById(R.id.rel4_1);
+    TextView rel42 = (TextView) findViewById(R.id.rel4_2);
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +39,6 @@ public class CrearActivity extends AppCompatActivity {
     }
 
     public void crearPregunta(View v) {
-
-        TextView titulo = (TextView) findViewById(R.id.tituloText);
-        TextView pregunta = (TextView) findViewById(R.id.preuntaText);
-
-        TextView rel11 = (TextView) findViewById(R.id.rel1_1);
-        TextView rel12 = (TextView) findViewById(R.id.rel1_2);
-
-        TextView rel21 = (TextView) findViewById(R.id.rel2_1);
-        TextView rel22 = (TextView) findViewById(R.id.rel2_2);
-
-        TextView rel31 = (TextView) findViewById(R.id.rel3_1);
-        TextView rel32 = (TextView) findViewById(R.id.rel3_2);
-
-        TextView rel41 = (TextView) findViewById(R.id.rel4_1);
-        TextView rel42 = (TextView) findViewById(R.id.rel4_2);
 
         Cursor c = db.rawQuery("SELECT * FROM preguntas ORDER BY id_pregunta DESC LIMIT 1", null);
 
@@ -59,6 +59,7 @@ public class CrearActivity extends AppCompatActivity {
         db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel31.getText()+"','"+rel32.getText()+"')");
         db.execSQL("INSERT INTO relaciones VALUES ('"+i+"','"+rel41.getText()+"','"+rel42.getText()+"')");
 
+        finish();
     }
 
     public void cancel(View v) {
